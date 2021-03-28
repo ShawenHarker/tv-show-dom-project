bf3b256f9daf4cf374b747a31a9611bf575b3f30
ShawenHarker <ShawenHarker@users.noreply.github.com>
2021-03-28 20:37:41 +0200

    Level 100 completed
    I got all the info needed on the page, I just need to edit my layout so that it looks more presentable.

--- script.js
+++ script.js
@@ -2,35 +2,28 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   const root = document.getElementById('root');
+  root.setAttribute('class', 'container');
   let makePageForEpisodes = allEpisodes.map((episode) => {
     let divTag = document.createElement('div'); 
     root.appendChild(divTag);
     divTag.setAttribute('class', 'card'); // creating a class called card for the div to be accessed with.
-    divTag.style.backgroundColor = "lightblue";
-    divTag.style.maxWidth = "18em";
-    divTag.style.padding = "1em"
-    divTag.style.margin = "5px";
     let imgTag = document.createElement('img');
-    imgTag.src = episode.image.medium;
     divTag.appendChild(imgTag);
+    imgTag.src = episode.image.medium;
     let h3Tag = document.createElement('h3');
     divTag.appendChild(h3Tag);
     h3Tag.innerText = episode.name;
-    h3Tag.style.textAlign = "center"; 
-    h3Tag.style.color = "black";
+    h3Tag.setAttribute('class', 'h3Tag');
     let h4Tag = document.createElement('h4');
     divTag.appendChild(h4Tag);
-    h4Tag.innerText = `S${episode.season}E${episode.number}`; 
-    h4Tag.style.textAlign = "center"; 
-    h4Tag.style.color = "black";
+    h4Tag.innerText = `S0${episode.season}E0${episode.number}`; 
+    h4Tag.setAttribute('class', 'h4Tag');
     let h4Tag2 = document.createElement('h4');
-    h4Tag2.innerHTML = episode.summary;
     divTag.appendChild(h4Tag2);
-    h4Tag2.style.color = "black";
-    let linkTag = document.createElement('link');
-    linkTag.href = episode._links.self.href;
-    divTag.appendChild(linkTag);
+    h4Tag2.innerHTML = episode.summary;
+    h4Tag2.setAttribute('class', 'h4Tag2');
   });
+  console.log(root);
 }
 
 window.onload = setup;
