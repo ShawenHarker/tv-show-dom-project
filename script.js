6e88e009e8df1b75e5efdebd7bdca37748e8741a
ShawenHarker <ShawenHarker@users.noreply.github.com>
2021-03-28 19:33:00 +0200

    Level 100
    Completed most of the requirements, need to clean up my JS and add all CSS in one file, add the links and set the proper code as required for the seasons.

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
-
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const root = document.getElementById('root');
+  let makePageForEpisodes = allEpisodes.map((episode) => {
+    let divTag = document.createElement('div'); 
+    root.appendChild(divTag);
+    divTag.setAttribute('class', 'card'); // creating a class called card for the div to be accessed with.
+    divTag.style.backgroundColor = "lightblue";
+    divTag.style.maxWidth = "18em";
+    divTag.style.padding = "1em"
+    divTag.style.margin = "5px";
+    let imgTag = document.createElement('img');
+    imgTag.src = episode.image.medium;
+    divTag.appendChild(imgTag);
+    let h3Tag = document.createElement('h3');
+    divTag.appendChild(h3Tag);
+    h3Tag.innerText = episode.name;
+    h3Tag.style.textAlign = "center"; 
+    h3Tag.style.color = "black";
+    let h4Tag = document.createElement('h4');
+    divTag.appendChild(h4Tag);
+    h4Tag.innerText = `S${episode.season}E${episode.number}`; 
+    h4Tag.style.textAlign = "center"; 
+    h4Tag.style.color = "black";
+    let h4Tag2 = document.createElement('h4');
+    h4Tag2.innerHTML = episode.summary;
+    divTag.appendChild(h4Tag2);
+    h4Tag2.style.color = "black";
+    let linkTag = document.createElement('link');
+    linkTag.href = episode._links.self.href;
+    divTag.appendChild(linkTag);
+  });
 }
 
 window.onload = setup;
